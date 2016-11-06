629d578c93fbe787f5d89999534d8a1d4c5c93c1
sandip <sandip.m@arthurdick.de>
2016-11-07 01:41:41 +0530

    port

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ var path = require("path");
 var express = require("express");
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 var publicPath = path.join(__dirname, "../", "/public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, function(){
+app.listen(port, function(){
 	console.log("Server running..");
 });
